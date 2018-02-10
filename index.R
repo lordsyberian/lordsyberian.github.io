1079f9fd4224135037e25dbb9aeec0c8bfea4d9e
lordsyberian <luan89saraiva@gmail.com>
2018-02-10 19:14:16 -0200

    Add files

---
+++ index.R
@@ -0,0 +1,3 @@
+require(rmarkdown)
+
+render(input="C:\\Users\\luan8\\Documents\\GitHub\\lordsyberian.github.io\\index.Rmd",output_file="C:\\Users\\luan8\\Documents\\GitHub\\lordsyberian.github.io\\index.html")
